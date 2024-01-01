813df1ac380f1e9b9ce85f56b59fbd153df980bf
TobeleroneBill <Billseaton19@gmail.com>
2024-01-01 15:04:27 +0000

    Mouse and Draw Calls

--- Game.py
+++ Game.py
@@ -2,13 +2,13 @@
 import pygame, random
 
 SIZE = (800,800)
-FPS = 60
+FPS = 144
 CLOCK = pygame.time.Clock()
 MouseRaw = [0,0]
 
 # Game Settings
 BoardSize = (3,3)
-
+CursorSize = 5
 
 # Game Functions
 class GameManager():
@@ -17,6 +17,10 @@ class GameManager():
         self.size = size
         self.screen = screen
         self.GetDividerSize()
+
+        self.mouseDelta = [0,0]
+        self.prevMouse = pygame.mouse.get_pos()
+
 
     def GetDividerSize(self):
         self.dividers = (self.screen.get_width()/self.size[0],
@@ -27,6 +31,7 @@ class GameManager():
         # Draw lines for each grid section
         Xpos = 0
         Ypos = 0
+        
         for x in range(0,self.size[0]-1):
             Xpos += self.dividers[0]
             pygame.draw.line(self.screen,(255,255,255),(Xpos,0),(Xpos,self.screen.get_height()))
@@ -34,6 +39,27 @@ class GameManager():
         for y in range(0,self.size[1]-1):
             Ypos += self.dividers[1]
             pygame.draw.line(self.screen,(255,255,255),(0,Ypos),(self.screen.get_width(),Ypos))
+
+    def DrawMouse(self):
+        # Switch to nice sprites at some point
+        match pygame.mouse.get_pressed():
+            case (1,0,0):
+                pygame.draw.circle(self.screen,(100,100,100),pygame.mouse.get_pos(),CursorSize)
+            case _:
+                pygame.draw.circle(self.screen,(255,255,255),pygame.mouse.get_pos(),CursorSize)
+
+    # Draw all screen things
+    def Draw(self):
+        self.DrawBoard()
+        self.DrawMouse()
+
+    # --------------------- Logic ---------------------    
+    def GetMouseDelta(self):
+        newpos = pygame.mouse.get_pos()
+        self.mouseDelta = (newpos[0] - self.prevMouse[0],newpos[1] - self.prevMouse[1])
+        print(self.mouseDelta)
+        self.prevMouse = newpos
+
 
 # For Fun
 def ChangingSize(GM):
@@ -63,8 +89,7 @@ def Game():
     # Game Loop
     while running:
         screen.fill((0,0,0))
-        GM.DrawBoard()
-        
+        GM.Draw()
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.WINDOWCLOSE or pygame.key.get_pressed()[pygame.K_ESCAPE]:
